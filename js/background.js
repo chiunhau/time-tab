add2aaca197e26760b75861112bca0090da7affa
Chiunhau <chiunhau9@gmail.com>
2015-07-17 13:10:04 +0800

    okay..

--- js/background.js
+++ js/background.js
@@ -1,23 +1,19 @@
-var UPDATE_INTERVAL = 3;//sec
+var UPDATE_INTERVAL = 5;//sec
 var IDLE_TIME = 30;//sec
 var apps;
 
 function updateLocal(domain) {
-  //first update the current apps
-  //then replace the local apps
-  var timeIncrement = UPDATE_INTERVAL;
   if (!apps[domain]) {
     console.log("going to create this domain: " + domain);
-    apps[domain] = { sumTime: timeIncrement }
+    apps[domain] = { sumTime: UPDATE_INTERVAL }
   }
   else {
-    apps[domain].sumTime += timeIncrement;
+    apps[domain].sumTime += UPDATE_INTERVAL;
   }
-
   localStorage.setItem('apps', JSON.stringify(apps));
 }
 
-function initial(callback) {
+function initial() {
   if (!localStorage.getItem('apps')) {
     console.log("no apps in local");
     localStorage.setItem('apps', JSON.stringify({}));
@@ -28,7 +24,6 @@ function initial(callback) {
 
 function update() {
   chrome.idle.queryState(IDLE_TIME, function(state) {
-    
     if (state === "active") {
       chrome.tabs.query({ "active": true, "lastFocusedWindow": true}, function(tabs) {
         if (tabs.length === 0) {
@@ -36,12 +31,10 @@ function update() {
           return;
         }
         else {
-          var tab = tabs[0];
-          chrome.windows.get(tab.windowId, function(currentWindow) {
+          chrome.windows.get(tabs[0].windowId, function(currentWindow) {
             if (currentWindow.focused == true) {
-              var domain = extractDomain(tab.url);
+              var domain = extractDomain(tabs[0].url);
               updateLocal(domain);
-              console.log("You Spent " + apps[domain].sumTime + "seconds on " + domain );
             }
           });
         }
@@ -54,13 +47,20 @@ function update() {
 }
 
 function extractDomain(url) {
-  var link = document.createElement('a');
-  link.href = url;
-
-  return link.hostname;
+  if (url.indexOf("file://") === 0) {
+    return "local_file"
+  }
+  else {
+    var link = document.createElement('a');
+    link.href = url;
+    if (link.hostname === "newtab") {
+      return "Awesome New Tab >///<"
+    }
+    else {
+      return link.hostname
+    }
+  }
 }
 
 initial();
-setInterval(function() {
-  update()
-}, 3000);
+setInterval(function() { update() }, UPDATE_INTERVAL * 1000);
--- js/newTab.js
+++ js/newTab.js
@@ -3,12 +3,10 @@ var apps = {};
 function loopOverApps(apps) {
 	var ary = [];
 	for (var key in apps) {
-		// console.log(key + ': ' + apps[key].sumTime);
 		if (apps.hasOwnProperty(key)) {
 			ary.push({domain: key, sumTime: apps[key].sumTime});
 		}
 	}
-	console.log(ary);
 	return ary;
 }
 
@@ -16,27 +14,6 @@ function update() {
 	apps = loopOverApps(JSON.parse(localStorage["apps"]));
 	apps.sort(function(a,b) { return b.sumTime - a.sumTime });
 	apps = apps.slice(0, 20);
-}
-
-function updateDraw() {
-	svg.selectAll('rect').data(apps).enter()
-		.append('rect')
-		.attr({'x': 5,
-					 'y': function(d, i) { return i * 22},
-					 'width': function(d, i) { return x(d.sumTime)},
-					 'height': 20,
-					 'fill': 'steelblue'
-	});
-
-	svg.selectAll('text').data(apps).enter()
-		.append('text')
-		.text( function(d) { return d.domain + '-' + d.sumTime})
-		.attr({
-			'x': 6,
-			'y': function(d, i) {return i * 22 + 14},
-			'fill': 'white',
-			'font-size': '12px'
-	});
 }
 
 update();
@@ -103,5 +80,4 @@ svg.selectAll('text.sum-time').data(apps).enter()
 		'font-size': '12px',
 		'class': function(d, i) { return 'data' + i }
 });
-
 
